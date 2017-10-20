b224dfe72b8dd75a7820ea8e72f8159ec55b2c99
MarAngelo <marangelo@Macs-MacBook-Air.local>
2017-10-21 01:34:29 +0800

    added git ignore

--- src/main/java/com/check/health/HealthCheck.java
+++ src/main/java/com/check/health/HealthCheck.java
@@ -5,9 +5,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Component
 @Path("api")
+@CrossOrigin("*")
 public class HealthCheck {
 	
 	@GET
